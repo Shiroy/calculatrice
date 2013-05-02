ed01a92682a7e1870d18a6d5cbee7a00cf4a7b99
cmercier <clemmercier@hotmail.com>
2013-05-02 20:13:16 +0200

    Ajout Multiplication

--- calculatrice.cpp
+++ calculatrice.cpp
@@ -5,18 +5,18 @@ using namespace std;
 int main()
 {
 	cout << "Entrez une operation (+ - * /)" << endl;
-	
+
 	char c;
 	cin >> c;
-	
+
 	cout << "Entrez le premier terme : ";
 	float a;
 	cin >> a;
-	
+
 	cout << "Entrez le second terme : ";
 	float b;
 	cin >> b;
-	
+
 	if(c == '+')
 	{
 		float resultat = a + b;
@@ -27,9 +27,15 @@ int main()
 		float resul = a-b;
 		cout << "a - b = " << result << endl;
 	}
-	
+
+	else if(c=='*')
+	{
+		float resul = a*b;
+		cout << "a * b = " << result << endl;
+	}
+
 	//TODO : Voir le bug tracker
-	
+
 	return 0;
 }
-	
+
